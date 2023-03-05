ca7cbc857f7ed8b3471b77815cadbd5c6d2f1fdf
GianMattiaPetroni <petronigianmattia@gmail.com>
2023-03-05 22:58:55 +0100

    esercizio 8 e metà del 9

---
+++ esercizio9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <array>
+
+using namespace std;
+int maxValore(int arrayInteri[], int arrayInteridue[] int grandezza)
+{ int valoreSuccessivo = 0;
+for (int i=0;i < grandezza; i++) {
+    for( int j = i + 1; j < grandezza; j++){
+        if (arrayInteri[i] == arrayinteridue[j]){
+            
+        }
+    }
+}
+
